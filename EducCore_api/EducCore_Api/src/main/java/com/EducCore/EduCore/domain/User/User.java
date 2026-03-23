00765ec714cbe962acfe4102f7e8ab77391d4dae
lucasdiniz-tecla <lucasdinizfrota@gmail.com>
2026-03-23 15:21:00 -0300

    Conexão banco de dados e back

--- EducCore_api/EducCore_Api/src/main/java/com/EducCore/EduCore/domain/User/User.java
+++ EducCore_api/EducCore_Api/src/main/java/com/EducCore/EduCore/domain/User/User.java
@@ -18,8 +18,8 @@ import java.util.List;
 @EqualsAndHashCode(of = "id")
 public class User implements UserDetails {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private String id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String name;
 
